98a509c2fc80135ec6126f07e61573c088ae58c3
Mateo Ortiz <jeusmateo@gmail.com>
2024-11-28 03:14:01 -0600

    modificado la bd para los arboles

--- vivero.sql
+++ vivero.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 28, 2024 at 06:07 AM
+-- Generation Time: Nov 28, 2024 at 10:12 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,6 +24,68 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `arboles`
+--
+
+CREATE TABLE `arboles` (
+  `id_arbol` int(11) NOT NULL,
+  `ruta_imagen` varchar(100) NOT NULL,
+  `id_familia` int(11) NOT NULL,
+  `nombre_cientifico` varchar(50) NOT NULL,
+  `nombre_comun` varchar(50) NOT NULL,
+  `descripcion` text NOT NULL,
+  `fruto` text NOT NULL,
+  `floracion` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `arboles`
+--
+
+INSERT INTO `arboles` (`id_arbol`, `ruta_imagen`, `id_familia`, `nombre_cientifico`, `nombre_comun`, `descripcion`, `fruto`, `floracion`) VALUES
+(1, '/images/test.jpg', 1, 'Pouteria campechiana (Kunth) Baehni', 'Kaniste’, guacume, mamey de Campeche', 'Árbol hasta de 27 metros de altura, con un tronco surcado, con la corona ancha y abierta. Hojas gruesamente membranáceas, oblanceoladas hasta obovadas, con un ápice obtuso y una base atenuada, con 11 pares de nervios laterales prominentes en el envés. Flores axilares o en nudos sin hojas, en fascículos con pocas flores o algunas veces solitarias, sépalos densamente seríceos, corola blanquecina, áspera, excediendo los sépalos.', 'amarillo, verde o café, un poco globoso, una a cuatro semillas largas, carnoso-amarillento, a menudo lechoso, dulce. ', 'Florece de marzo a mayo. Se reproduce por semilla.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `arboles_usos`
+--
+
+CREATE TABLE `arboles_usos` (
+  `id_uso` int(11) NOT NULL,
+  `id_arbol` int(11) NOT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `detalles` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `arboles_usos`
+--
+
+INSERT INTO `arboles_usos` (`id_uso`, `id_arbol`, `nombre`, `detalles`) VALUES
+(1, 1, 'Comestible', 'Los frutos son consumidos crudos y tienen un sabor exótico, contienen azúcares, vitaminas y sales minerales.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `familia_arboles`
+--
+
+CREATE TABLE `familia_arboles` (
+  `id_familia` int(11) NOT NULL,
+  `nombre` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `familia_arboles`
+--
+
+INSERT INTO `familia_arboles` (`id_familia`, `nombre`) VALUES
+(1, 'SAPOTACEAE');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `usuarios`
 --
 
@@ -39,6 +101,26 @@ CREATE TABLE `usuarios` (
 --
 
 --
+-- Indexes for table `arboles`
+--
+ALTER TABLE `arboles`
+  ADD PRIMARY KEY (`id_arbol`),
+  ADD KEY `id_familia` (`id_familia`);
+
+--
+-- Indexes for table `arboles_usos`
+--
+ALTER TABLE `arboles_usos`
+  ADD PRIMARY KEY (`id_uso`),
+  ADD KEY `id_arbol` (`id_arbol`);
+
+--
+-- Indexes for table `familia_arboles`
+--
+ALTER TABLE `familia_arboles`
+  ADD PRIMARY KEY (`id_familia`);
+
+--
 -- Indexes for table `usuarios`
 --
 ALTER TABLE `usuarios`
@@ -49,10 +131,44 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT for table `arboles`
+--
+ALTER TABLE `arboles`
+  MODIFY `id_arbol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `arboles_usos`
+--
+ALTER TABLE `arboles_usos`
+  MODIFY `id_uso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `familia_arboles`
+--
+ALTER TABLE `familia_arboles`
+  MODIFY `id_familia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `usuarios`
 --
 ALTER TABLE `usuarios`
   MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `arboles`
+--
+ALTER TABLE `arboles`
+  ADD CONSTRAINT `arboles_ibfk_1` FOREIGN KEY (`id_familia`) REFERENCES `familia_arboles` (`id_familia`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `arboles_usos`
+--
+ALTER TABLE `arboles_usos`
+  ADD CONSTRAINT `arboles_usos_ibfk_1` FOREIGN KEY (`id_arbol`) REFERENCES `arboles` (`id_arbol`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
