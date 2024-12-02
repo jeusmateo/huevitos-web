b9a562b9f2813dbb5a94bee8873242f554b72cd1
Mateo Ortiz <jeusmateo@gmail.com>
2024-12-01 21:20:38 -0600

    minor fixes

--- Php/leer.php
+++ Php/leer.php
@@ -12,4 +12,4 @@ $sql = "SELECT * FROM arbol_descripcion";
 $arboles = ejecutar_sql_configurado($sql);
 http_response_code(200);
 header('Content-type: application/json');
-echo json_encode($arboles, JSON_FORCE_OBJECT);
+echo json_encode($arboles, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE);
